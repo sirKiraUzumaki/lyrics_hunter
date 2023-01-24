efda010bdecdd759e502715e41899315540dc850
Ayush Lakra <48526201+sirKiraUzumaki@users.noreply.github.com>
2023-01-24 23:00:28 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
 	name="lyrics-hunter",
-	version="0.0.1",
+	version="1.0.0",
 	author="Ayush Lakra",
 	author_email="ayushlakra846@gmail.com",
 	packages=["lyrics-hunter", "lyrics-hunter.hunter"],
