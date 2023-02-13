06da6f579d7a7a4567da7ab24d6f9c930fd41ec1
Ayush Lakra <48526201+sirKiraUzumaki@users.noreply.github.com>
2023-02-13 14:48:18 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,14 @@
-import setuptools
+from setuptools import find_packages, setup
 
-with open("README.md", "r") as fh:
-	description = fh.read()
-
-setuptools.setup(
-	name="lyrics-hunter",
-	version="1.0.0",
-	author="Ayush Lakra",
-	author_email="ayushlakra846@gmail.com",
-	packages=["lyrics-hunter", "lyrics-hunter.hunter"],
-	description="Python Lyrics Scrapper",
-	long_description="Lyrics Hunter is a python library to scrape lyrics from websites. It is a web scraping library designed to extract song lyrics from websites. The library currently only supports scraping from AZLyrics but in near future, it will support more websites.",
-	long_description_content_type="text/markdown",
-	url="https://github.com/sirKiraUzumaki/lyrics_hunter",
-	license='MIT',
-	python_requires='>=3.7',
-	install_requires=['beautifulsoup4', 'requests']
+setup(
+    name='lyrics_hunter',
+    packages=find_packages(),
+    version='10.0.0',
+    description='Python Lyrics Scrapper',
+    author='Ayush Lakra',
+    author_email="ayushlakra846@gmail.com",
+    license='MIT',
+    install_requires=['beautifulsoup4', 'requests'],
+    test_requires=['beautifulsoup4', 'requests'],
+    test_suite='tests',
 )
